0a8225d7e2746ef712caed14c39ee8e92da1cb2c
vamsip33 <vamsip33@gmail.com>
2023-12-13 15:31:45 +0530

    update Dev back end

--- java/shipping-dashboard/shippingdashboard-service/src/main/java/com/wds/ship/controller/ExportDocumentationServiceController.java
+++ java/shipping-dashboard/shippingdashboard-service/src/main/java/com/wds/ship/controller/ExportDocumentationServiceController.java
@@ -1,8 +1,35 @@
 package com.wds.ship.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wds.ship.entity.ExportDocumentation;
+import com.wds.ship.service.ExportDocumentationService;
+import com.wds.ship.shared.lcl.export.CustomerServicePOJO;
+import com.wds.ship.shared.user.UserDetails;
+
 @RestController
+@RequestMapping("/api/v1/export")
 public class ExportDocumentationServiceController {
+	
+	@Autowired
+    ExportDocumentationService exportDocumentService;
+	
+	@GetMapping("/getExportDocumentService")
+    public List<ExportDocumentation> findByUserId(@RequestParam(name = "userId") Long userId) {
+        return exportDocumentService.getExportDocumentation();
+    }
+   
+    @PostMapping("/getExportDocumentServiceInfo")
+    public List<CustomerServicePOJO> getExportDocumentServiceInfo(@RequestBody UserDetails userDetails) {
+        return exportDocumentService.getExportDocumentationServiceInfo(userDetails);
+    }
 
 }
