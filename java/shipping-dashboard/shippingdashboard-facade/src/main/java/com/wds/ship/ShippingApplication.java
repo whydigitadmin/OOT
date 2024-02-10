0070fd0fd7c8256cca497f5306de126704fd6c0b
mvenkadesh2020 <kvenkadesan@whydigit.com>
2024-02-10 15:13:52 +0530

    Security update

--- java/shipping-dashboard/shippingdashboard-facade/src/main/java/com/wds/ship/ShippingApplication.java
+++ java/shipping-dashboard/shippingdashboard-facade/src/main/java/com/wds/ship/ShippingApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShippingApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ShippingApplication.class, args);
 	}
 
